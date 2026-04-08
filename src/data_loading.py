b98195b975bf12d77c3e1bc6286826cc052ca2f6
Vincent <C24498082@myTUDublin.ie>
2026-04-08 20:27:40 +0100

    implemented requests to dataloading.py

--- src/data_loading.py
+++ src/data_loading.py
@@ -1,15 +1,38 @@
 import pandas as pd
-from bs4 import beautifulsoup
+from bs4 import BeautifulSoup
 import requests
 
+def LoadCsv():
+    """Loads csv file of Malin Head from Met Eireann"""
+    df = pd.read_csv('data/raw/dly1575.csv', skiprows= 24)
+
+
+def ScrapeClimateAverages():
+    """ Scrapes 30 year climate normals table from Met EIreanns Malin Head page"""
+    url = "https://www.met.ie/cms/assets/uploads/2023/09/www_met_ie_malin_9120.htm"
+
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text,'html.parser')
+
+    table = soup.find("table")
+
+    rows = table.find_all("tr")
+
+    print(response.text)
+#data = []
+#for row in rows:
+#    cols = row.find_all(["td","th"])
+#    cols = [col.text.strip() for col in cols]
+#    data.append(cols)
+
+#df = pd.DataFrame(data)
+
+    print(f"Status code: {response.status_code}\n")
 
 
 
-df = pd.read_csv('data/raw/dly1575.csv', skiprows= 24)
-
-print(df.head())
-print(df.shape)
 
 
-
-print(df.isnull().sum())
+#print(df.isnull().sum())
+LoadCsv()
+ScrapeClimateAverages()
