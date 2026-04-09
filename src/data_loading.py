16cba045c8134e14d2a983a01116fd5cfb7fcabf
Vincent <C24498082@myTUDublin.ie>
2026-04-09 11:48:03 +0100

    completed web scraping for data_loading.py

--- src/data_loading.py
+++ src/data_loading.py
@@ -5,12 +5,12 @@ import requests
 def LoadCsv():
     """Loads csv file of Malin Head from Met Eireann"""
     df = pd.read_csv('data/raw/dly1575.csv', skiprows= 24)
-
+    
+    return df
 
 def ScrapeClimateAverages():
     """ Scrapes 30 year climate normals table from Met EIreanns Malin Head page"""
     url = "https://www.met.ie/cms/assets/uploads/2023/09/www_met_ie_malin_9120.htm"
-
     response = requests.get(url)
     soup = BeautifulSoup(response.text,'html.parser')
 
@@ -18,21 +18,31 @@ def ScrapeClimateAverages():
 
     rows = table.find_all("tr")
 
-    print(response.text)
-#data = []
-#for row in rows:
-#    cols = row.find_all(["td","th"])
-#    cols = [col.text.strip() for col in cols]
-#    data.append(cols)
-
-#df = pd.DataFrame(data)
-
     print(f"Status code: {response.status_code}\n")
+    
+    if response.ok:
+        print(f"Succesfully fetched: {url}\n")
+
+        data=[]
+        rows = soup.find_all("tr")
+        for row in rows:
+           
+           cols = [c.get_text(strip=True) for c in row.find_all("td")]
+
+           if len(cols) == 14:
+                data.append(cols)
+
+        scraped_data = data[1:]
+        
+        return scraped_data
 
 
-
-
-
-#print(df.isnull().sum())
 LoadCsv()
-ScrapeClimateAverages()
+print("*" * 50)
+print("\nSuccesfully laoded csv file\n")
+
+print("*" *50)
+print("\nScraped raw data - Malin Head\n")
+print("*" *50)
+
+print(ScrapeClimateAverages())
--- src/preprocessing.py
+++
@@ -1,3 +0,0 @@
-
-
-df['date'] = pd.to_datetime(df['date'])
