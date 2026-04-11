0b35416224da9cfd8b3fdb9db36b90c0325e8be1
Vincent <C24498082@myTUDublin.ie>
2026-04-11 11:54:36 +0100

    drop all ind columns in clean_daily_data()

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,32 +1,32 @@
 import pandas as pd
 import numpy as np
+from data_loading import ScrapeClimateAverages
+from data_loading import LoadCsv
 
-def clean_dataframe(df):
+
+def clean_daily_data(df):
     """Clean the raw dataframe of Malin Head
         - convert to correct data format
         - engineer new features
     """
+    df['date'] = pd.to_datetime(df['date'])
 
+    #drop all "ind" columns  
+    ind_cols = [col for col in df.columns if col.startswith('ind')]
+    df = df.drop(columns = ind_cols)
 
-
-def clean_scraped_data:
-data=[]
-        rows = soup.find_all("tr")
-        for row in rows:
-           
-           cols = [c.get_text(strip=True) for c in row.find_all("td")]
-
-           if len(cols) == 14:
-                data.append(cols)
-
-        clean_data = data[1:]
     
-        header = data[1]
-
-        df = pd.DataFrame(clean_data,  columns= header)
-        
 
 
 
 
-df['date'] = pd.to_datetime(df['date'])
+
+
+def clean_scraped_data():
+    """Cleans scraped 30 year climate normals
+        - return most relevant rows
+        - reshape dataframe (each month is a colmn rather than row)
+    """
+
+
+
