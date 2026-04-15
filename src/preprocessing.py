44bfdd86d2c65aa9f7971d3e80e4a753de0dc0fa
Vincent <C24498082@myTUDublin.ie>
2026-04-15 16:06:09 +0100

    added data cleaning for scraped data

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -7,17 +7,35 @@ from data_loading import LoadCsv
 def clean_daily_data(df):
     """Clean the raw dataframe of Malin Head
         - convert to correct data format
-        - engineer new features
+        - engineer new features (month,year, meantp)
     """
+    #convert date column to proper datetime
     df['date'] = pd.to_datetime(df['date'])
 
     #drop all "ind" columns  
     ind_cols = [col for col in df.columns if col.startswith('ind')]
     df = df.drop(columns = ind_cols)
 
+    #create month and yer columns
+    df['month'] = df['date'].dt.month
+    df['year'] = df['date'].dt.year
+
+    malin_head_column = ['maxtp', 'mintp','gmin', 'rain', 'cbl',
+                         'wdsp', 'sun', 'soil', 'pe', 'evap']
+
+    for col in malin_head_column:
+        if col in df.columns:
+            df[col] = pd.to_numeric(df[col], errors = 'coerce')
+
+    #create a mean temp column- average of max and min
+
+    df['meantp'] = (df['maxtp'] + df['mintp']) / 2
     
+    print(f" Data ranges between: {df['date'].min()} to {df['date'].max()} \n")
+    print(f"Total rows: {len(df)} \n")
+    print(f"Amount of missing values : {df.isnull().sum}")
 
-
+    return df
 
 
 
@@ -28,5 +46,46 @@ def clean_scraped_data():
         - reshape dataframe (each month is a colmn rather than row)
     """
 
+    raw_normals_df = ScrapeClimateAverages()
 
 
+    #relevant row labels from scraped table
+    desired_rows ={
+        'mean daily max': 'normals_maxtp',
+        'mean daily min': 'normals_mintp',
+        'mean temperature': 'normals_meantp',
+        'mean monthly total': 'normals_rain',
+        'mean monthly speed': 'normals_wdsp',
+        'mean daily duration': 'normals_sun',
+    }
+
+    month_cols = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']
+
+
+
+
+    clean_dictionary = {} #dictionary which will store cleaned data
+
+    for index, row in raw_normals_df.iterrows():
+        label = row.iloc[0].strip() # strip first column of each row is the variable name
+        for key in desired_rows: #loop through dictionary
+            if key in label.lower():
+                values = pd.to_numeric(row[month_cols], errors='coerce').tolist()
+                clean_dictionary[desired_rows[key]] = values
+    
+    # turn dictionary into dataframe where each row is a month
+    result = pd.DataFrame(clean_dictionary)
+    
+
+    print("*" * 50)
+    print("\nDataframe post-cleaning :\n")
+    print("*" * 50)
+
+    print(result)
+
+clean_scraped_data()
+
+df = LoadCsv()
+cleaned_csv = clean_daily_data(df)
+
+print(cleaned_csv)
