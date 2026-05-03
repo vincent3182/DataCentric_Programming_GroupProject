eeb08557729db108430789686075deecf2eefbcc
Vincent <C24498082@myTUDublin.ie>
2026-05-03 16:26:23 +0100

    altered data_loading to suit new dataset, added new air quality dataset,covnverted to numeric values in preprocessing.py

--- src/data_loading.py
+++ src/data_loading.py
@@ -1,56 +1,18 @@
 import pandas as pd
-from bs4 import BeautifulSoup
-import requests
+
 
 def LoadCsv():
-    """Loads csv file of Malin Head from Met Eireann"""
-    df = pd.read_csv('data/raw/dly1575.csv', skiprows= 24)
+    """Loads csv file of Air Quality dataset fromm data/raw folder"""
+    df = pd.read_csv('data/raw/global_air_quality_dataset.csv')
     
+    print("*"* 50)
+    print("Succesfully laoded CSV File")
+
+    print(df.shape)
     return df
 
-def ScrapeClimateAverages():
-    """ Scrapes 30 year climate normals table from Met EIreanns Malin Head page"""
-    url = "https://www.met.ie/cms/assets/uploads/2023/09/www_met_ie_malin_9120.htm"
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text,'html.parser')
 
-    table = soup.find("table")
 
-    rows = table.find_all("tr")
 
-    print(f"Status code: {response.status_code}\n")
-    
-    if response.ok:
-        print(f"Succesfully fetched: {url}\n")
-
-        data=[]
-        rows = soup.find_all("tr")
-        for row in rows:
-           
-           cols = [c.get_text(strip=True) for c in row.find_all("td")]
-
-           if len(cols) == 14:
-                data.append(cols)
-
-        scraped_data = data[2:]
-        
-        column_names = data[1]
-
-        Normals30Yr_df = pd.DataFrame(scraped_data, columns = column_names)        
-        return Normals30Yr_df
-        #return scraped_data
-
-daily_df = LoadCsv()
-
-#print(daily_df.head())
-#print(daily_df.columns.tolist())
-
-print("*" * 50)
-print("\nSuccesfully laoded csv file\n")
-
-print("*" *50)
-print("\nScraped raw data - Malin Head\n")
-print("*" *50)
-
-normals_df = ScrapeClimateAverages()
-#print(normals_df)
+if __name__ == "__main__":
+    LoadCsv()
--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,103 +1,50 @@
 import pandas as pd
-from data_loading import ScrapeClimateAverages
 from data_loading import LoadCsv
 
 
 def clean_daily_data(df):
-    """Clean the raw dataframe of Malin Head
+    """Clean the raw dataframe of Air Quality
         - convert to correct data format
-        - engineer new features (month,year, meantp)
+        - engineer new features (month,year, seasons)
+        - convert values to numbers
+        -handle missing values and outliers
     """
-    #convert date column to proper datetime
-    df['date'] = pd.to_datetime(df['date'])
+    
+    #rename Columns to simplify
 
-    #drop all "ind" columns  
-    ind_cols = [col for col in df.columns if col.startswith('ind')]
-    df = df.drop(columns = ind_cols)
+
+
+    df = df.rename(columns = renamed_columns)
+
+
+
+
+
+    #convert date column to proper datetime
+    df['date'] = pd.to_datetime(df['date'], format = '%y-%m-%d')
 
     #create month and yer columns
     df['month'] = df['date'].dt.month
     df['year'] = df['date'].dt.year
 
-    malin_head_column = ['maxtp', 'mintp','gmin', 'rain', 'cbl',
-                         'wdsp', 'sun', 'soil', 'pe', 'evap']
 
+    #Convert Measurements to Numbers
+    malin_head_column = ['aqi', 'pm25','pm10', 'no2', 'so2',
+                         'co', 'o3', 'temperature', 'humidity', 'wind_speed']
+
+    #coerce bad values to NAN
     for col in malin_head_column:
         if col in df.columns:
             df[col] = pd.to_numeric(df[col], errors = 'coerce')
 
     #create a mean temp column- average of max and min
 
-    df['meantp'] = (df['maxtp'] + df['mintp']) / 2
     
     print(f" Data ranges between: {df['date'].min()} to {df['date'].max()} \n")
     print(f"Total rows: {len(df)} \n")
     
+
     return df
 
 
 
-
-def clean_scraped_data():
-    """Cleans scraped 30 year climate normals
-        - return most relevant rows
-        - reshape dataframe (each month is a colmn rather than row)
-    """
-
-    raw_normals_df = ScrapeClimateAverages()
-
-
-    #relevant row labels from scraped table
-    desired_rows ={
-        'mean daily max': 'normals_maxtp',
-        'mean daily min': 'normals_mintp',
-        'mean temperature': 'normals_meantp',
-        'mean monthly total': 'normals_rain',
-        'mean monthly speed': 'normals_wdsp',
-        'mean daily duration': 'normals_sun',
-    }
-
-    month_cols = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']
-
-
-
-
-    clean_dictionary = {} #dictionary which will store cleaned data
-
-    for index, row in raw_normals_df.iterrows():
-        label = row.iloc[0].strip() # strip first column of each row is the variable name
-        for key in desired_rows: #loop through dictionary
-            if key in label.lower():
-                values = pd.to_numeric(row[month_cols], errors='coerce').tolist()
-                clean_dictionary[desired_rows[key]] = values
-    
-    # turn dictionary into dataframe where each row is a month
-    result = pd.DataFrame(clean_dictionary)
-    
-
-    print("*" * 50)
-    print("\nDataframe post-cleaning :\n")
-    print("*" * 50)
-
-    print(result)
-    return result
-
-cleaned_normals = clean_scraped_data()
-
-df = LoadCsv()
-cleaned_csv = clean_daily_data(df)
-
-#print(cleaned_csv)
-
-def export_clean_data(daily_df,normals_df):
-    """exports both cleaned dataframes to processed folder as CSV"""
-
-    daily_df.to_csv('data/processed/daily_clean.csv', index= False)
-    print("exported: daily_clean.csv to /processed")
-    print("*"*50)
-
-    normals_df.to_csv('data/processed/normals_clean.csv', index= False)
-    print("exported: normals_clean.csv to /processed")
-    print("*"*50)
-
-export_clean_data(cleaned_csv,cleaned_normals)
